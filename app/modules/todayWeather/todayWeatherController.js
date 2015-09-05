cfeeab9bb2e128716741d9aa66da7e597770de02
gordon_lin <gordon_lin@mail.gss.com.tw>
2015-09-05 10:53:21 +0800

    transform weather data detect 404 error

--- app/modules/todayWeather/todayWeatherController.js
+++ app/modules/todayWeather/todayWeatherController.js
@@ -1,5 +1,17 @@
 angular.module('app.todayWeather.controller', ['app.todayWeather.service'])
-.controller('TodayWeatherController', [function() {
-	this.city = '';
-	this.country = '';
-}]);
+	.controller('TodayWeatherController', [
+		'WeatherService',
+		'$scope',
+		function (weatherService, $scope) {
+			$scope.weather = {};
+			$scope.city = 'taipei';
+			$scope.country = 'tw';
+			$scope.search = function () {
+				weatherService.search($scope.city, $scope.country).success(function (data) {
+					$scope.weather = data;
+					console.log(data);
+				}).error(function(message) {
+					console.log(message);
+				});
+			}
+		}]);
--- app/modules/todayWeather/weatherService.js
+++ app/modules/todayWeather/weatherService.js
@@ -1,6 +1,21 @@
 angular.module('app.todayWeather.service', [])
-.service('WeatherService', ['$http', function($http) {
-	this.getTodayWeather = function(city, country) {
-		return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&units=metric');
-	}
-}]);
+	.service('WeatherService', ['$http', function ($http) {
+		this.search = function (city, country) {
+			var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&units=metric';
+			return $http.get(url, {
+				transformResponse: $http.defaults.transformResponse.concat([function (data) {
+					if(data.cod && data.cod === '404') {
+						throw 'location not found';
+					}
+					var result = {
+						main: data.weather[0].main,
+						description: data.weather[0].description,
+						tempMax: data.main.temp_max,
+						tempMin: data.main.temp_min,
+						humidity: data.main.humidity
+					}
+					return result;
+				}])
+			});
+		}
+	}]);
