4ec065d9e41c7bd71e5a5d1fe994f293c59c61a2
gordon_lin <gordon_lin@mail.gss.com.tw>
2015-09-05 11:03:19 +0800

    show weather info (todo: weather img)

--- app/modules/todayWeather/todayWeatherController.js
+++ app/modules/todayWeather/todayWeatherController.js
@@ -4,14 +4,17 @@ angular.module('app.todayWeather.controller', ['app.todayWeather.service'])
 		'$scope',
 		function (weatherService, $scope) {
 			$scope.weather = {};
+			$scope.loaded = false;
 			$scope.city = 'taipei';
 			$scope.country = 'tw';
 			$scope.search = function () {
 				weatherService.search($scope.city, $scope.country).success(function (data) {
 					$scope.weather = data;
 					console.log(data);
+					$scope.loaded = true;
 				}).error(function(message) {
 					console.log(message);
+					$scope.loaded = false;
 				});
 			}
 		}]);
